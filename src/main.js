834a6048df05238ac5cfb5d9702e6079ef224811
Filipe Natanael <filipenatanael@live.com>
2018-11-08 19:37:19 -0200

    chore: webpack testing configuration

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import './plugins/vuetify-assets'
 
-import './plugins/vuetify'
 import App from './App.vue'
 import { routes } from './routes'
 import store from './store'
 
 Vue.config.productionTip = false
-
 Vue.use(VueRouter)
+Vue.use(Vuetify)
 
 const router = new VueRouter({
   routes,
---
+++ src/plugins/vuetify-assets.js
@@ -0,0 +1,3 @@
+import 'vuetify/src/stylus/app.styl'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
